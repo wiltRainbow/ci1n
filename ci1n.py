fb3ab1d5f768d7246c4c75564e3fabe6511ee703
wilt <109252692+wiltRainbow@users.noreply.github.com>
2022-08-07 14:18:32 +0200

    source file

---
+++ ci1n.py
@@ -0,0 +1,29 @@
+"""compress into 1 number (ci1n)
+allows you to compress multiple numbers into one
+"""
+from math import ceil
+
+BIT_SHIFTING_AMOUNT = 7
+MAX_VALUE = 2**BIT_SHIFTING_AMOUNT - 1
+
+
+def compress(input_numbers: list[int]):
+    compressed = 0
+    for number in input_numbers:
+        if number > MAX_VALUE:
+            raise ValueError(f"{number} > {MAX_VALUE}({BIT_SHIFTING_AMOUNT})")
+        if number < 0:
+            raise ValueError("Cannot handle negative values")
+        compressed <<= BIT_SHIFTING_AMOUNT
+        compressed += number
+
+    return compressed
+
+
+def decompress(compressed: int):
+    output_numbers = []
+    for i in range(ceil(len(f"{compressed:b}")/BIT_SHIFTING_AMOUNT)):
+        output_numbers.insert(0, compressed & MAX_VALUE)
+        compressed >>= BIT_SHIFTING_AMOUNT
+
+    return output_numbers
